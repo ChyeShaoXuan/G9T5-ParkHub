b9a62b1e290d74e0031df8bcfb1b11b88f6e5f6b
pehhk <146614956+pehhk@users.noreply.github.com>
2024-03-15 17:13:17 +0800

    user

--- user.py
+++ user.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import IntegrityError
 from os import environ
 
 app = Flask(__name__)
@@ -7,6 +8,7 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = (
     environ.get("dbURL") or "mysql+mysqlconnector://root@localhost:3306/user"
 )
+# mysql+mysqlconnector://is213@host.docker.internal:3306/user in compose.yaml
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
@@ -17,25 +19,34 @@ class User(db.Model):
     __tablename__ = 'user'
 
 
-    userID = db.Column(db.Integer, primary_key=True)
+    userID = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(64), nullable=False)
     email = db.Column(db.String(120), nullable=False)
     password = db.Column(db.String(64), nullable=False)
     phoneNo = db.Column(db.Integer, nullable=False)
-    notifAllowed = db.Column(db.Boolean, default=False)
 
 
-    def __init__(self, userID, name, email, password, phoneNo, notifAllowed):
-        self.userID = userID
-        self.name = name
-        self.email = email
-        self.password = password
-        self.phoneNo = phoneNo
-        self.notifAllowed = notifAllowed
+    # def __init__(self, userID, name, email, password, phoneNo, notifAllowed):
+    #     self.userID = userID
+    #     self.name = name
+    #     self.email = email
+    #     self.password = password
+    #     self.phoneNo = phoneNo
+    #     self.notifAllowed = notifAllowed
+
+
+    # def json(self):
+    #     return {"userID": self.userID, "name": self.name, "email": self.email, "password": self.password, "phoneNo": self.phoneNo, "notifAllowed": self.notifAllowed}
 
 
     def json(self):
-        return {"userID": self.userID, "name": self.name, "email": self.email, "password": self.password, "phoneNo": self.phoneNo, "notifAllowed": self.notifAllowed}
+        return {
+            "userID": self.userID,
+            "name": self.name,
+            "email": self.email,
+            "password": self.password,
+            "phoneNo": self.phoneNo,
+        }
 
 
 @app.route("/user")
@@ -59,7 +70,7 @@ def get_all():
         }
     ), 404
 
-@app.route("/user/<integer:userID>")
+@app.route("/user/<int:userID>")
 def find_by_userID(userID):
     user = db.session.scalars(
     	db.select(User).filter_by(userID=userID).
@@ -81,94 +92,69 @@ def find_by_userID(userID):
         }
     ), 404
 
-@app.route("/user/<integer:userID>", methods=['POST'])
-def create_user(userID):
-    if (db.session.scalars(
-    	db.select(User).filter_by(userID=userID).
-    	limit(1)
-).first()
-):
-        return jsonify(
-            {
-                "code": 400,
-                "data": {
-                    "userID": userID
-                },
-                "message": "User already exists."
-            }
-        ), 400
-
+@app.route("/user", methods=['POST'])
+def create_user():
 
     data = request.get_json()
-    user = User(userID, **data)
 
+    # Check if email already exists
+    if db.session.query(User).filter_by(email=data.get('email')).first():
+        return jsonify({"code": 400, "message": "User with this email already exists."}), 400
+
+    # Create user object
+    user = User(**data)
 
     try:
         db.session.add(user)
         db.session.commit()
-    except:
-        return jsonify(
-            {
-                "code": 500,
-                "data": {
-                    "userID": userID
-                },
-                "message": "An error occurred creating the user."
-            }
-        ), 500
+
+    except IntegrityError as e:
+        db.session.rollback()
+        return jsonify({"code": 400, 
+                        "message": "Integrity Error: " + str(e)}
+                        ), 400
+    
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"code": 500, 
+                        "message": "An error occurred creating the user: " + str(e)}
+                        ), 500
+
+    return jsonify({"code": 201, 
+                    "data": user.json()}
+                    ), 201
 
 
-    return jsonify(
-        {
-            "code": 201,
-            "data": user.json()
-        }
-    ), 201
-
-
-@app.route("/user/<integer:userID>", methods=['POST'])
-def create_user(userID):
-    if (db.session.scalars(
-    	db.select(User).filter_by(userID=userID).
-    	limit(1)
-).first()
-):
-        return jsonify(
-            {
-                "code": 400,
-                "data": {
-                    "userID": userID
-                },
-                "message": "User already exists."
-            }
-        ), 400
-
-
+@app.route("/user/<int:user_id>", methods=['PUT'])
+def update_user(user_id):
     data = request.get_json()
-    user = User(userID, **data)
 
+    # Retrieve the user from the database
+    user = User.query.get(user_id)
+
+    if user is None:
+        return jsonify({"code": 404, "message": "User not found."}), 404
+
+    # Update user attributes
+    for key, value in data.items():
+        setattr(user, key, value)
 
     try:
-        db.session.add(user)
         db.session.commit()
-    except:
-        return jsonify(
-            {
-                "code": 500,
-                "data": {
-                    "userID": userID
-                },
-                "message": "An error occurred creating the user."
-            }
-        ), 500
+    except IntegrityError as e:
+        db.session.rollback()
+        return jsonify({"code": 400, 
+                        "message": "Integrity Error: " + str(e)}
+                        ), 400
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"code": 500, 
+                        "message": "An error occurred updating the user: " + str(e)}
+                        ), 500
 
-
-    return jsonify(
-        {
-            "code": 201,
-            "data": user.json()
-        }
-    ), 201
+    return jsonify({"code": 200, 
+                    "message": "User updated successfully."}
+                    ), 200
 
 
 if __name__ == '__main__':
