2429af9028aca1de48275fcf1e4b86610ed56097
Chye Shao Xuan <142379429+ChyeShaoXuan@users.noreply.github.com>
2024-03-25 22:40:13 +0800

    Update locateCP.js

--- static/js/locateCP.js
+++ static/js/locateCP.js
@@ -110,7 +110,7 @@ async function getCoordsForAddress(address) {
 
         const coordinates = data.results[0].geometry.location;
         $.ajax({ 
-            url: 'http://localhost:5002/handle_coords', //  searchInfo/handle_coords
+            url: 'http://localhost:5000/google_results', //  sent straight to googlewrapper instead of having to go through search info 
             type: 'POST', 
             contentType: 'application/json', 
             data: JSON.stringify({ 'value': coordinates }), 
