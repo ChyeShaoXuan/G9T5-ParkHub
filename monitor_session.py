4da90264472bc8745008fa72af556275567ebdfc
jermaineclx <147256123+jermaineclx@users.noreply.github.com>
2024-03-28 13:40:27 +0800

    Update monitor_session.py

--- monitor_session.py
+++ monitor_session.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 import os, sys
+from os import environ
 
 import requests
 from invokes import invoke_http
@@ -10,14 +11,27 @@ import pika
 import json
 import amqp_connection
 
+# APScheduler runs in background to trigger jobs at regular 5-min intervals to monitor_session
+from flask_apscheduler import APScheduler
+import datetime
+
+scheduler = APScheduler()
+
 app = Flask(__name__)
+
+scheduler.init_app(app)
+
+scheduler.start()
+
+import logging
+logging.getLogger('apscheduler').setLevel(logging.DEBUG)
+
 CORS(app)
 
-user_URL = "http://localhost:5000/user"
-session_URL = "http://localhost:5001/session/trigger"
-# shipping_record_URL = "http://localhost:5002/shipping_record"
-# activity_log_URL = "http://localhost:5003/activity_log"
-# error_URL = "http://localhost:5004/error"
+# user_URL = "http://localhost:5000/user"
+# session_URL = "http://localhost:5001/session/trigger"
+user_URL = environ.get('user_URL') or "http://user:5010/user" 
+session_URL = environ.get('session_URL') or "http://session:5006/session/trigger" 
 
 exchangename = "notification_topic" # exchange name
 exchangetype="topic"
@@ -30,13 +44,12 @@ if not amqp_connection.check_exchange(channel, exchangename, exchangetype):
     sys.exit(0)  # Exit with a success status
 
 
-
 @app.route("/monitor_session")
 def monitor_session():
     # Simple check of input format and data of the request are JSON
     try:
             # do the actual work
-            # 1. Send order info {cart items}
+            # get ending sessions
         result = processMonitorSession()
         return jsonify(result), result["code"]
 
@@ -54,120 +67,68 @@ def monitor_session():
 
     # if reached here, not a JSON request.
     
-
-
 def processMonitorSession():
-    # 2. Send the order info {cart items}
-    # Invoke the order microservice
-    # print('\n-----Invoking user microservice-----')
-    # user_result = invoke_http(user_URL, method='GET')
-    # print('user_result:', user_result)
-
-    # 4. Record new order
-    # record the activity log anyway
-    print('\n\n-----Invoking session_record microservice-----')
+    # Invoke the session microservice
+    print('\n\n-----Invoking session microservice-----')
     sessions_results = invoke_http(session_URL)
+    print("Sessions Results:", json.dumps(sessions_results, indent=4))  # This will print the structure of the sessions_results
+    logging.info("Checking for sessions that are about to end...")
     print("\nExpiring Sessions retrieved\n", sessions_results)
-    list_of_ending_session = [endingsession for endingsession in sessions_results['data']]
+    if sessions_results['code'] == 200:
 
-    for info in list_of_ending_session:
-        userID = info['userID']
-        sessionID = info['sessionID']
-        endtime = info['endtime']
-        print("hiiiiiiii")
-        user_info = invoke_http(user_URL+'/'+str(userID))
-        print(type(user_info))
-        # user_info = json.loads(user_info)
-        print(user_info)
-        phoneNo = user_info['data']['phoneNo']
-        print('\n\n-----Publishing the (order info) message with routing_key=notification.send-----')        
+        list_of_ending_session = [endingsession for endingsession in sessions_results['data']]
 
-        message = {
-            'sessionID':sessionID, 
-            'phoneNo':phoneNo, 
-            'endtime':endtime
-            } # need sessionID, phoneNo, endtime
-        messagejson = json.dumps(message)
-        # invoke_http(activity_log_URL, method="POST", json=order_result)            
-        channel.basic_publish(exchange=exchangename, routing_key="notification.send", 
-            body=messagejson, properties=pika.BasicProperties(delivery_mode = 2))
-        
+        for info in list_of_ending_session:
+            notifAllowed = info['notifAllowed']
+            if notifAllowed == 1:
+                userID = info['userID']
+                sessionID = info['sessionID']
+                endtime = info['endtime']
+                user_info = invoke_http(user_URL+'/'+str(userID))
+                phoneNo = user_info['data']['phoneNo']
+                print('\n\n-----Publishing the (notification info) message with routing_key=notification.send-----')        
 
-    
-    # 4. Record new order
-    # record the activity log anyway
-    #print('\n\n-----Invoking activity_log microservice-----')
-        
-    
-    print("\nOrder published to RabbitMQ Exchange.\n")
-        
+                message = {
+                    'sessionID':sessionID, 
+                    'phoneNo':phoneNo, 
+                    'endtime':endtime
+                    } # need sessionID, phoneNo, endtime
+                messagejson = json.dumps(message) # convert message to json format
+       
+                channel.basic_publish(exchange=exchangename, routing_key="notification.send", 
+                    body=messagejson, properties=pika.BasicProperties(delivery_mode = 2))
 
+                print("\nOrder published to RabbitMQ Exchange.\n")
 
-    # - reply from the invocation is not used;
-    # continue even if this invocation fails
-
-    # Check the order result; if a failure, send it to the error microservice.
-    # code = user_result["code"]
-    # if code not in range(200, 300):
-
-    #     # Inform the error microservice
-    #     print('\n\n-----Invoking error microservice as order fails-----')
-    #     invoke_http(error_URL, method="POST", json=order_result)
-    #     # - reply from the invocation is not used; 
-    #     # continue even if this invocation fails
-    #     print("Order status ({:d}) sent to the error microservice:".format(
-    #         code), order_result)
-
-    #     # 7. Return error
-    #     return {
-    #         "code": 500,
-    #         "data": {"order_result": order_result},
-    #         "message": "Order creation failure sent for error handling."
-    #     }
-
-    # 5. Send new order to shipping
-    # Invoke the shipping record microservice
-    # print('\n\n-----Invoking shipping_record microservice-----')
-    # shipping_result = invoke_http(
-    #     shipping_record_URL, method="POST", json=order_result['data'])
-    # print("shipping_result:", shipping_result, '\n')
-
-    # # Check the shipping result; 
-    # # if a failure, send it to the error microservice.
-    # code = shipping_result["code"]
-    # if code not in range(200, 300):
-
-    #     # Inform the error microservice
-    #     print('\n\n-----Invoking error microservice as shipping fails-----')
-    #     invoke_http(error_URL, method="POST", json=shipping_result)
-    #     print("Shipping status ({:d}) sent to the error microservice:".format(
-    #         code), shipping_result)
-
-    #     # 7. Return error
-    #     return {
-    #         "code": 400,
-    #         "data": {
-    #             "order_result": order_result,
-    #             "shipping_result": shipping_result
-    #         },
-    #         "message": "Simulated shipping record error sent for error handling."
-    #     }
-
-    # 7. Return created order, shipping record
     return {
         "code": 201,
         "data": {
-            # "user_result": user_result,
             "sessions_results": sessions_results
         }
     }
 
+def schedule_monitor_session():
+    scheduler.add_job(id='monitor_session_job', func=monitor_session, trigger='interval', minutes=5)
 
+# This checks the next run time of the scheduler, which should run every 5 min
+@app.route('/next_run_time')
+def next_run_time():
+    try:
+        job = scheduler.get_job('monitor_session_job')
+        if job:
+            return jsonify({
+                'next_run_time': job.next_run_time.strftime('%Y-%m-%d %H:%M:%S') if next_run_time else 'N/A'
+            })
+        else:
+            return jsonify({'error': 'Job not found'}), 404
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 # Execute this program if it is run as a main script (not by 'import')
 if __name__ == "__main__":
     print("This is flask " + os.path.basename(__file__) +
           " for placing an order...")
+    schedule_monitor_session()
     app.run(host="0.0.0.0", port=5100, debug=True)
     # Notes for the parameters:
     # - debug=True will reload the program automatically if a change is detected;
